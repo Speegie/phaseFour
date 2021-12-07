07166d5b453f329a27044a0ad2e243ebc2a14bd8
Speegie <speegie.ian@gmail.com>
2021-12-07 14:39:57 -0500

    app.py, index.html, users.html, setDate.html
    These are the base files, they aren't finalized at all

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+from datetime import date
+
+app = Flask(__name__)
+
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+currentDate = date.today()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        #cur = mysql.connection.cursor()
+        #cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+        #mysql.connection.commit()
+        #cur.close()
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM accounts")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html', userDetails=userDetails)
+
+@app.route('/setDate')
+def setDate():
+    if request.method == 'POST':
+        currentDate = request.form
+        theCurrentDate = currentDate['totalDate']
+    return render_template('setDate.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
